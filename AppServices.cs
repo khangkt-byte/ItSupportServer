f1f3fc66e9fce00af8e5103a94482757cb85a34e
IT4 <IT4@IT3>
2026-01-17 14:42:29 +0700

    Add project files.

---
+++ AppServices.cs
@@ -0,0 +1,13 @@
+﻿namespace ITSupportServer
+{
+    public static class AppServices
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services)
+        {
+            // Register application services here
+            //services.AddScoped<IExampleService, ExampleService>();
+
+            return services;
+        }
+    }
+}
